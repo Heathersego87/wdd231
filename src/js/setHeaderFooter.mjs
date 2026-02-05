b345aa71083728989b674bf7fd516fb4425adb79
heathersego87 <heathersego87@yahoo.com>
2026-02-05 17:40:31 -0600

    W05 Prove: add conditions page, alerts, visitor centers, activities, and update Vite build inputs

--- src/js/setHeaderFooter.mjs
+++ src/js/setHeaderFooter.mjs
@@ -1,31 +1,25 @@
 import { parkInfoTemplate, footerTemplate } from "./templates.mjs";
 
 function setHeaderInfo(data) {
-  // disclaimer link
+  // insert data into disclaimer section
   const disclaimer = document.querySelector(".disclaimer > a");
   disclaimer.href = data.url;
   disclaimer.innerHTML = data.fullName;
-
-  // page title
+  // update the title of the site. Notice that we can select things in the head just like in the body with querySelector
   document.querySelector("head > title").textContent = data.fullName;
-
-  // hero image
-  const heroImg = document.querySelector(".hero-banner__image");
-  heroImg.src = data.images[0].url;
-  heroImg.alt = data.images[0].altText || data.images[0].title || data.fullName;
-
-  // hero text (use .hero-banner__info so you don't overwrite the wrapper)
-  const heroInfo = document.querySelector(".hero-banner__info");
-  heroInfo.innerHTML = parkInfoTemplate(data);
+  // set the banner image
+  document.querySelector(".hero-banner > img").src = data.images[0].url;
+  // use the template function above to set the rest of the park specific info in the header
+  document.querySelector(".hero-banner__content").innerHTML =
+    parkInfoTemplate(data);
 }
 
 function setFooter(data) {
-  // your footer has id="park-footer" (and also class="park-footer"), so either works
   const footerEl = document.querySelector("#park-footer");
-  footerEl.innerHTML = footerTemplate(data);
+  footerEl.insertAdjacentHTML("afterbegin", footerTemplate(data));
 }
 
-export default function setHeaderFooter(data) {
-  setHeaderInfo(data);
-  setFooter(data);
+export default function setHeaderFooter(parkData) {
+  setHeaderInfo(parkData);
+  setFooter(parkData);
 }
