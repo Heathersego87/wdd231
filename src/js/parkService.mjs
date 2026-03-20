52abae27bd99a8006fd9de9d47633923d6d97a63
heathersego87 <heathersego87@yahoo.com>
2026-03-20 13:43:43 -0500

    Add visitor center detail page with dynamic data

--- src/js/parkService.mjs
+++ src/js/parkService.mjs
@@ -228,7 +228,7 @@ export function getInfoLinks(data) {
 }
 
 export async function getParkData() {
-  const parkData = await getJson("parks?parkCode=yell");
+  const parkData = await getJson("parks?parkCode=yell ");
   return parkData.data[0];
 }
 
@@ -240,4 +240,9 @@ export async function getParkAlerts(code) {
 export async function getParkVisitorCenters(code) {
   const parkData = await getJson(`visitorcenters?parkCode=${code}`);
   return parkData.data;
+}
+
+export async function getParkVisitorCenterDetails(id) {
+  const parkData = await getJson(`visitorcenters?id=${id}`);
+  return parkData.data[0];
 }
